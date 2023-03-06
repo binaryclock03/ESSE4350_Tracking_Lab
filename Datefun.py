b17f48524d7e8da884b16bd78c59691e02ed6c45
Daniel <binaryclock03@gmail.com>
2023-03-06 15:05:34 -0500

    Datefun.doy() finished

--- Datefun.py
+++ Datefun.py
@@ -1,5 +1,9 @@
-def doy():
-    pass
+import datetime as dt
+
+def doy(year, month, day):
+    d0 = dt.date(year, 1, 1)
+    d1 = dt.date(year, month, day)
+    return (d1 - d0).days
 
 def frcofd():
     pass
