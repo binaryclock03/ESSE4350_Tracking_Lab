21dae1c9dedcc957dd259547190dbecf4b087c5f
Daniel <binaryclock03@gmail.com>
2023-03-06 16:50:17 -0500

    created satellite class

---
+++ Satellite.py
@@ -0,0 +1,16 @@
+class Satellite():
+    name:str
+    refepoch:float
+    incl:float
+    raan:float
+    eccn:float
+    arg_per:float
+    mean_an:float
+    mean_mo:float
+    ndot2:float
+    nddot6:float
+    bstar:float
+    orbitnum:float
+
+    def __init__():
+        pass
