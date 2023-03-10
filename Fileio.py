be74bf9ac8e91f39af233df441716696511487fc
binaryclock03 <binaryclock003@gmail.com>
2023-03-09 19:44:36 -0500

    added sattelite class and finished up ReadNoradTLE

--- Fileio.py
+++ Fileio.py
@@ -1,8 +1,9 @@
-from Station import *
+from Station import Station
+from Satellite import Satellite
 
 def banner():
     ver = 0.1
-    welcome_msg = "Hello we are the two idots called Luke and Daniel"
+    welcome_msg = "Hello we are the two called Luke and Daniel"
     print(f"ESSE4350 2023-03-06 v{ver}\n{welcome_msg}")
 
 def anykey():
@@ -14,5 +15,5 @@ def errmsg(error_str):
 def ReadStationFile(station_file_path) -> Station:
     return Station(station_file_path)
 
-def ReadNoradTLE():
-    pass
+def ReadNoradTLE(sattelite_file_path) -> Satellite:
+    return Satellite(sattelite_file_path)
--- Satellite.py
+++ Satellite.py
@@ -10,7 +10,18 @@ class Satellite():
     ndot2:float
     nddot6:float
     bstar:float
-    orbitnum:float
+    orbitnum:int
 
-    def __init__():
-        pass
+    def __init__(self, sattelite_file_path):
+        with open(sattelite_file_path) as file:
+            lines = file.readlines()
+
+            self.name = str(lines[0][2:]).replace("\n", "")
+            self.refepoch = float(lines[1][16:30])
+            self.incl = float(lines[2][7:16])
+            self.raan = float(lines[2][16:25])
+            self.eccn = float("0." + lines[2][25:33])
+            self.arg_per = float(lines[2][33:42])
+            self.mean_an = float(lines[2][42:51])
+            self.mean_mo = float(lines[2][51:61])
+            self.orbitnum = int(lines[2][61:66])
